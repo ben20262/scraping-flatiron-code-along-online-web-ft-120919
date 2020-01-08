ccd208b4368cfa9f7b82a85d9b92506b18cb57ae
Benjamin Small <ben20262@aim.com>
2020-01-08 19:37:18 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,11 @@
+class Course
+  attr_accessor :title, :schedule, :description
+  @@all = []
 
-
+  def initialize(title, schedule, description)
+    @title = title
+    @schedule = schedule
+    @description = description
+    @@all << self
+  end
+end
