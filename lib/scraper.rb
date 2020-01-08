e5ee96ee098ffde27a887feadd9e777cc138bc8d
Benjamin Small <ben20262@aim.com>
2020-01-08 19:50:59 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,4 +19,8 @@ class Scraper
   def get_page
     Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end
+
+  def get_courses
+    get_page.css(".post")
+  end
 end
